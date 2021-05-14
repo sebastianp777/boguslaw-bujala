09bca1444c2dfef994efcc35a771c413be168c04
DESKTOP-6DCOJ77\chrza <chrzanowski.mariusz1@gmail.com>
2021-05-14 18:01:51 +0200

    update

--- wp-content/themes/boguslawBujalaTheme/functions.php
+++ wp-content/themes/boguslawBujalaTheme/functions.php
@@ -26,7 +26,8 @@ if (function_exists('add_theme_support')) {
     // Add Thumbnail Theme Support
     add_theme_support('post-thumbnails');
     add_image_size('large', 700, '', true); // Large Thumbnail
-    add_image_size('medium', 250, '', true); // Medium Thumbnail
+    add_image_size('cover', 309, '', true); // Cover image Thumbnail
+    add_image_size('medium', 309, '', true); // Medium Thumbnail
     add_image_size('small', 120, '', true); // Small Thumbnail
     add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 
--- wp-content/themes/boguslawBujalaTheme/page.php
+++ wp-content/themes/boguslawBujalaTheme/page.php
@@ -12,7 +12,7 @@
 				<!-- article -->
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-					<h2 class="title__h2"><?php the_title(); ?></h2>
+					<!-- <h2 class="title__h2"><?php the_title(); ?></h2> -->
 					<?php the_content(); ?>
 
 					<br class="clear">
